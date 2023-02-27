0ead5dac3c12debd9d1bf72e1bf44cb50811f771
dawid mrotek <DawidMrotek@protonmail.ch>
2023-02-28 00:31:31 +0100

    add ASGI_APPLICATION

--- django_chat/django_chat/settings.py
+++ django_chat/django_chat/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'django_chat.wsgi.application'
-
+ASGI_APPLICATION = 'django_chat.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases

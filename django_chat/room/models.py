1624c14a40d55204dbc175ca63e24e2b06651f26
dawid mrotek <DawidMrotek@protonmail.ch>
2023-03-01 22:39:17 +0100

    create room model

--- django_chat/room/models.py
+++ django_chat/room/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Room(models.Model):
+    # long name of the room
+    name = models.CharField(max_length=255)
+    # short name of the room used in url
+    slug = model.SlugField(unique=True)

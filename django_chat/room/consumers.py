39bf3c81c4f18490883cb22c2c05ad0b05e822b5
dawid mrotek <DawidMrotek@protonmail.ch>
2023-03-02 06:19:18 +0100

    create consumer

---
+++ django_chat/room/consumers.py
@@ -0,0 +1,33 @@
+import json
+
+# class for creating consumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+# for storing things into database inside asynchronous view
+from agriref.sync import sync_to_async
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    # handle connection
+    async def connect(self):
+        # select correct room based on url
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        # setup group name
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # join channel
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            # created automagically
+            self.channel_name
+        )
+
+        # wait for connection to server
+        await self.accept()
+    
+    # handle disconnect
+    async def disconnect(self):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            # created automagically
+            self.channel_name
+        )
